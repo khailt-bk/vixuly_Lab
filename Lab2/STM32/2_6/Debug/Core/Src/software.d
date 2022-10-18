2aabd25fe109ade2cd40cd4cfbf1542f4f4c571d
Le Thanh Khai <khai.lethanh@hcmut.edu.vn>
2022-10-18 20:45:25 +0700

    update code for Lab2 til ex6

--- Lab2/STM32/2_6/Debug/Core/Src/software.d
+++
@@ -1,3 +0,0 @@
-Core/Src/software.o: ../Core/Src/software.c ../Core/Inc/software.h
-
-../Core/Inc/software.h:
--- Lab2/STM32/2_8/Debug/Core/Src/software_timmer.d
+++
@@ -1,4 +0,0 @@
-Core/Src/software_timmer.o: ../Core/Src/software_timmer.c \
- ../Core/Inc/software_timmer.h
-
-../Core/Inc/software_timmer.h:
